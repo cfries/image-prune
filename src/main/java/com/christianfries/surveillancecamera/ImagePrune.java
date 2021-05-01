476dde44cd74c85dd0e659e28bbaac4787e02bce
Christian Fries <email@christian-fries.de>
2021-05-01 19:12:07 +0200

    Code cleanup.

--- src/main/java/com/christianfries/surveillancecamera/ImagePrune.java
+++ src/main/java/com/christianfries/surveillancecamera/ImagePrune.java
@@ -1,44 +1,39 @@
 package com.christianfries.surveillancecamera;
 
-import java.awt.Color;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 import javax.imageio.ImageIO;
 
 public class ImagePrune {
 
-
 	public static void main(String[] args) throws IOException, InterruptedException {
-		if(args.length != 2) throw new IllegalArgumentException("Please provide path to images and threshold as arguments.");
-		String directory = args[0];
-		Double threshold = Double.valueOf(args[1]);
+		if(args.length != 2) {
+			throw new IllegalArgumentException("Please provide path to images and threshold as arguments.");
+		}
+		final String directory = args[0];
+		final Double threshold = Double.valueOf(args[1]);
 
-		File directoryPath = new File(directory);
+		final File directoryPath = new File(directory);
 		// List of all files and directories
-		String contents[] = directoryPath.list();
+		final String contents[] = directoryPath.list();
 		Arrays.sort(contents);
 
-		ExecutorService fileDeletionService = Executors.newSingleThreadExecutor();
+		final ExecutorService fileDeletionService = Executors.newSingleThreadExecutor();
 
 		BufferedImage reference = null;
 		for(int i=0; i<contents.length; i++) {
 
 			try {
 				// Load the images
-				BufferedImage image = ImageIO.read(new File(directory + File.separator + contents[i]));
+				final BufferedImage image = ImageIO.read(new File(directory + File.separator + contents[i]));
 
-				double level = new ImageCompare().getImageDifference(reference, image);
+				final double level = new ImageCompare().getImageDifference(reference, image);
 				reference = image;
 
 				System.out.print(contents[i] + "\t" + i + "\t" + level);
@@ -48,8 +43,8 @@ public class ImagePrune {
 					final String filename = contents[i];
 					final String pathname = directory + File.separator + contents[i];
 					fileDeletionService.submit(() -> {
-						File f= new File(pathname);           //file to be delete  
-						if(!f.delete()) {  
+						final File f= new File(pathname);           //file to be delete
+						if(!f.delete()) {
 							System.out.println(filename + " deletion failed.");
 						}
 					});
@@ -58,7 +53,7 @@ public class ImagePrune {
 					System.out.println(" ok");
 				}
 			}
-			catch(Exception e) {
+			catch(final Exception e) {
 				e.printStackTrace();
 			}
 		}
--- src/main/java/com/christianfries/surveillancecamera/TakePictureUponChange.java
+++ src/main/java/com/christianfries/surveillancecamera/TakePictureUponChange.java
@@ -1,9 +1,6 @@
 package com.christianfries.surveillancecamera;
 
-import java.awt.Color;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -12,16 +9,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.lang.ProcessBuilder.Redirect;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 import javax.imageio.ImageIO;
 
@@ -32,24 +23,24 @@ public class TakePictureUponChange {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-		Double threshold = Double.valueOf(args[0]);
+		final Double threshold = Double.valueOf(args[0]);
 		final String	filenamePrefix = args[1];
 		final String	targetDir = args[2];
 		final String	imageCommand = args[3];
 
-		ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash");
+		final ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash");
 		processBuilder.directory(null);
 		processBuilder.redirectError(new File(TakePictureUponChange.class.getSimpleName() + ".log"));
-		Process process = processBuilder.start();
+		final Process process = processBuilder.start();
 
-		OutputStream stdin = process.getOutputStream();
-		InputStream stdout = process.getInputStream();
+		final OutputStream stdin = process.getOutputStream();
+		final InputStream stdout = process.getInputStream();
 
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-		BufferedReader reader = new BufferedReader (new InputStreamReader(stdout));
+		final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+		final BufferedReader reader = new BufferedReader (new InputStreamReader(stdout));
 
 		// Launch image script, will save image to filename and write filename to reader.
-		String script = ""
+		final String script = ""
 				+ "for(( ; ; ))\n"
 				+ "do\n"
 				+ "  timestamp=$(date +%s.%3N)\n"
@@ -66,50 +57,50 @@ public class TakePictureUponChange {
 
 		while(true) {
 			try {
-				String filename = reader.readLine();
+				final String filename = reader.readLine();
 				// Load the image
-				long timeReadStart = System.currentTimeMillis();
+				final long timeReadStart = System.currentTimeMillis();
 				final BufferedImage image = ImageIO.read(new File(filename));
-				long timeReadEnd = System.currentTimeMillis();
+				final long timeReadEnd = System.currentTimeMillis();
 				//				System.out.println("Read....: " + ((timeReadEnd-timeReadStart)/1000.0));
 
 				final BufferedImage referenceImage = reference;
 				executorImageCompare.submit(() -> saveWhenDifferent(referenceImage, image, threshold, filename, targetDir));
 				reference = image;
 			}
-			catch(Exception e) {
+			catch(final Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
 	private static boolean saveWhenDifferent(final BufferedImage reference, final BufferedImage image, double threshold, String filename, String targetDir) throws IOException {
-		long timeCompareStart = System.currentTimeMillis();
-		double level = new ImageCompare().getImageDifference(reference, image);
-		long timeCompareEnd = System.currentTimeMillis();
+		final long timeCompareStart = System.currentTimeMillis();
+		final double level = new ImageCompare().getImageDifference(reference, image);
+		final long timeCompareEnd = System.currentTimeMillis();
 
 		final double timeCompare = ((timeCompareEnd-timeCompareStart)/1000.0);
 		final boolean isDifferent = level > threshold;
 
 		executorFileTransfer.submit(() -> {
 			try {
-				long timeCleanStart = System.currentTimeMillis();
+				final long timeCleanStart = System.currentTimeMillis();
 				if(level > threshold) {
-					String target = targetDir + File.separator + filename;
+					final String target = targetDir + File.separator + filename;
 					Files.copy(Paths.get(filename), Paths.get(target));
 					Files.delete(Paths.get(filename));
 				}
 				else {
 					Files.delete(Paths.get(filename));
 				}
-				long timeCleanEnd = System.currentTimeMillis();
+				final long timeCleanEnd = System.currentTimeMillis();
 				final double timeTransfer = (timeCleanEnd-timeCleanStart)/1000.0;
 
 				System.out.println(
 						filename + "\t" + String.format("%8.5f", level) + "\t" +
-						"\t" + String.format("%-10s (compare: %5.3f s, transfer: %5.3f s).", ((level > threshold) ? "transfered" : "deleted"), timeCompare, timeTransfer));
+								"\t" + String.format("%-10s (compare: %5.3f s, transfer: %5.3f s).", ((level > threshold) ? "transfered" : "deleted"), timeCompare, timeTransfer));
 			}
-			catch(Exception e)
+			catch(final Exception e)
 			{
 				System.out.println(filename + "\t" + level + "\tFAILED.");
 				e.printStackTrace();
